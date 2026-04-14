0cf957a5f252c0f768e1b3aa90cc790d76401a91
Tomas <cristiantomasvegaappelqvist@gmail.com>
2026-04-14 01:08:36 +0100

    Readme changes and low fix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // Aquí guardamos la IP de tu PC como si fuera un contacto en el móvil
   apiUrl: 'http://' + window.location.hostname + ':8080/api/'
 };
